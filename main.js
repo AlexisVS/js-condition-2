840216288025f9919951eb96a693320e5a33e550
AlexisVS <63750707+AlexisVS@users.noreply.github.com>
2021-04-20 09:44:31 +0200

    base

---
+++ main.js
@@ -0,0 +1,72 @@
+//exo 1
+// let nombre1 = +prompt("Tape un nombre");
+// let nombre2 = +prompt("Tape un nombre");
+// if (nombre1 == nombre2) {
+//     alert(`Le nombre ${nombre1} et le
+//     nombre ${nombre2} sont-ils égaux ? \n réponse : oui`)
+// }
+// else {
+//     alert(`Le nombre ${nombre1} et le
+//     nombre ${nombre2} sont-ils égaux ? \n réponse : non`)
+// }
+//exo 2
+
+// let nombre1 = +prompt("Tape un nombre");
+// let nombre2 = +prompt("Tape un nombre");
+// if (nombre1 < nombre2) {
+//     alert(`Le nombre ${nombre1} est il plus petit que le
+//     nombre ${nombre2} ? \n réponse : oui`)
+// }
+// else {
+//     alert(`Le nombre ${nombre1} est il plus petit que le
+//     nombre ${nombre2} ? \n réponse : non`)
+// }
+
+//exo 3
+// let nombre1 = +prompt("Tape un nombre");
+// let nombre2 = +prompt("Tape un nombre");
+// let nombre3 = +prompt("Tape un nombre");
+// let comparaison = nombre1 + nombre2 > nombre3
+// if (nombre1 + nombre2 > nombre3) {
+//     alert(`La somme du nombre x
+//     + le nombre y est-elle plus grande que le nombre z ? : ${comparaison}`)
+// }
+// else {
+//     alert(`La somme du nombre x
+//     + le nombre y est-elle plus grande que le nombre z ? : reponse : non`)
+// }
+
+//exo 4
+
+// let phrase = prompt("Tape une phrase random");
+// let nombreCaractere = phrase.length;
+// let estimation = +prompt("Combien de caractere compte ta phrase");
+// let difference = Math.abs(phrase - nombreCaractere);
+
+// if (nombreCaractere == estimation) {
+//     alert("Bien joué tu as la bonne réponse")
+// }
+// else {
+//     alert(`Réponse fausse tu es a ${difference} unités de la bonne réponse`)
+// }
+
+// exo 5
+
+// let prenom = prompt("Ecrit ton prenom")
+// if (prenom == undefined) {
+//     alert("Tu na pas ecrit ton prenom ecrit le !!!!")
+//     prenom = prompt("Ecrit ton prenom")
+// }
+// else {
+//     alert(`Bonjour ${prenom}`)
+// }
+
+//exo 6
+
+let abonnement = promp("Voulez vous vous abonnez ?");
+if (abonnement.toLowerCase() == "oui") {
+
+}
+else {
+    prompt("En êtes vous certain ?");
+}
